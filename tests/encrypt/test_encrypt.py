d23495d3589cfc626490855b6197c77458837221
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-25 16:03:30 -0300

    feat(req02): criação dos testes para função de codificação de um string

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('SAPO', 2) == 'OP_AS'
+    assert encrypt_message('SAPO', 3) == 'PAS_O'
+    assert encrypt_message('SAPO', 8) == 'OPAS'
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('SAPO', '1')
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(2, 3)
