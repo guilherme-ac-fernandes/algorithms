b06eac363606e36ac6bbcc2d1ecc254b7e681f93
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-25 23:04:04 -0300

    feat(req06): criação da função interativa para validação de palavras palíndromas

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    start = 0
+    end = len(word) - 1
+
+    while start <= end:
+        print(word[start], word[end])
+        if word[start] == word[end]:
+            start += 1
+            end -= 1
+        else:
+            return False
+    return True
