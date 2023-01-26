01a5ebd11bb6f6eac1352e361e2fe276553d8c9a
Guilherme Fernandes <gfengquimica@gmail.com>
2023-01-25 22:43:21 -0300

    feat(req05): implementa função para encontrar número repetidos em lista

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) <= 1:
+        return False
+
+    numbers = sorted(nums)
+    for index, num in enumerate(numbers[:-1]):
+        if not isinstance(num, int) or num < 0:
+            return False
+        if num == numbers[index + 1]:
+            return num
+    return False
